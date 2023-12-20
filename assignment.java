bcca885ee129a5577607a0d36db3558618599d5a
Rakesh Kumar <rakesh18146@gmail.com>
2023-12-20 23:50:08 +0530

    mutabel String

---
+++ assignment.java
@@ -0,0 +1,14 @@
+// What is IMutable String in Java Explain with an Example
+
+public class assignment {
+    public static void main(String[] args) {
+        String rakesh = "rakesh";
+        System.out.println(rakesh);
+        rakesh = rakesh.concat("gupta");
+        System.out.println(rakesh);
+
+
+        
+    }
+    
+}
